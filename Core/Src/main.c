c0b9b4cc7da69898e262c587612b1a93d697ffd7
Siddharth Kharvi <Siddharthkharvi69756@gmail.com>
2023-11-17 21:20:10 +0530

    1.1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,22 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
+#define I2C_ADDR 0x27 // I2C address of the PCF8574
+#define RS_BIT 0 // Register select bit
+#define EN_BIT 2 // Enable bit
+#define BL_BIT 3 // Backlight bit
+#define D4_BIT 4 // Data 4 bit
+#define D5_BIT 5 // Data 5 bit
+#define D6_BIT 6 // Data 6 bit
+#define D7_BIT 7 // Data 7 bit
+
+#define LCD_ROWS 2 // Number of rows on the LCD
+#define LCD_COLS 16 // Number of columns on the LCD
+
+uint8_t backlight_state = 1;
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -76,10 +91,17 @@ static void MX_I2C3_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-
+void lcd_write_nibble(uint8_t nibble, uint8_t rs);
+void lcd_send_cmd(uint8_t cmd);
+void lcd_send_data(uint8_t data);
+void lcd_init();
+void lcd_write_string(char *str);
+void lcd_set_cursor(uint8_t row, uint8_t column);
+void lcd_clear(void);
+void lcd_backlight(uint8_t state);
 
 /* USER CODE END 0 */
-
+void getinput();
 /**
   * @brief  The application entry point.
   * @retval int
@@ -123,14 +145,105 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  lcd_init();
+  lcd_backlight(1); // Turn on backlight
+  uint16_t Data = 10;
+  char *text = "EmbeddedThere";
+  char int_to_str[10];
+  sprintf(int_to_str, "%d", Data);
+
+  lcd_clear();
+  lcd_set_cursor(0, 0);
+  lcd_write_string(text);
+  lcd_write_string(int_to_str);
+
+  while (1){
     /* USER CODE END WHILE */
+
 
     /* USER CODE BEGIN 3 */
 
   }
   /* USER CODE END 3 */
+}
+
+void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
+  uint8_t data = nibble << D4_BIT;
+  data |= rs << RS_BIT;
+  data |= backlight_state << BL_BIT; // Include backlight state in data
+  data |= 1 << EN_BIT;
+  HAL_I2C_Master_Transmit(&hi2c3, I2C_ADDR << 1, &data, 1, 100);
+  HAL_Delay(1);
+  data &= ~(1 << EN_BIT);
+  HAL_I2C_Master_Transmit(&hi2c3, I2C_ADDR << 1, &data, 1, 100);
+}
+
+void lcd_send_cmd(uint8_t cmd) {
+  uint8_t upper_nibble = cmd >> 4;
+  uint8_t lower_nibble = cmd & 0x0F;
+  lcd_write_nibble(upper_nibble, 0);
+  lcd_write_nibble(lower_nibble, 0);
+  if (cmd == 0x01 || cmd == 0x02) {
+    HAL_Delay(2);
+  }
+}
+
+void lcd_send_data(uint8_t data) {
+  uint8_t upper_nibble = data >> 4;
+  uint8_t lower_nibble = data & 0x0F;
+  lcd_write_nibble(upper_nibble, 1);
+  lcd_write_nibble(lower_nibble, 1);
+}
+
+void lcd_init() {
+  HAL_Delay(50);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(5);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x02, 0);
+  lcd_send_cmd(0x28);
+  lcd_send_cmd(0x0C);
+  lcd_send_cmd(0x06);
+  lcd_send_cmd(0x01);
+  HAL_Delay(2);
+}
+
+void lcd_write_string(char *str) {
+  while (*str) {
+    lcd_send_data(*str++);
+  }
+}
+
+void lcd_set_cursor(uint8_t row, uint8_t column) {
+    uint8_t address;
+    switch (row) {
+        case 0:
+            address = 0x00;
+            break;
+        case 1:
+            address = 0x40;
+            break;
+        default:
+            address = 0x00;
+    }
+    address += column;
+    lcd_send_cmd(0x80 | address);
+}
+
+void lcd_clear(void) {
+	lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+void lcd_backlight(uint8_t state) {
+  if (state) {
+    backlight_state = 1;
+  } else {
+    backlight_state = 0;
+  }
 }
 
 /**
